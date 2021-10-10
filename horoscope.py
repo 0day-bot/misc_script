8c8df769775b6115864adae57799be4a1f628192
Ian <51884387+0day-bot@users.noreply.github.com>
2021-10-10 13:03:44 -0400

    Create horoscope.py

---
+++ horoscope.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import requests
+
+def get_horoscope(ZODIAC):
+
+    url = f"https://www.astrology.com/horoscope/daily/{ZODIAC}.html"
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    hor = soup.find_all('p')
+
+    return hor
+
+def main():
+    hor = get_horoscope('taurus')
+    daily = hor[1].get_text()
+    love = hor[2].get_text()
+    work = hor[3].get_text()
+
+    print(f"Daily Horoscope: \n {daily}\nLove Horoscope: \n {love}\nWork Horoscope: \n{work}")
+
+if __name__ == "__main__":
+    main()
